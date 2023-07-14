116e91fa731a2f0afa6611027fe7af2e2cc11e2a
JonnerPaz <jonnerpazp@gmail.com>
2023-07-14 19:12:02 -0400

    Solve welcome.js!

---
+++ 8kyu/welcome.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const languages = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso',
+};
+
+function greet(language) {
+  const { english } = languages;
+  if (!languages[language] || typeof language !== 'string') return english;
+  return languages[language];
+}
